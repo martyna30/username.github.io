31f687c374d42769e6c4bbb25b784b911e1ef019
martyna30 <prawdzikmartyna@op.pl>
2021-01-15 08:07:25 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    const apiRoot = 'http://localhost:8080/v1/library';
+    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
+    const $booksContainer = $('[data-books-container]');
+
+    let availableBoards = {};
+    let availableBooks = {};
+
+    $('[data-book-add-form]').on(
+        'submit',
+        function() {
+            $.ajax({
+                url: apiRoot + '/createBook',
+                method: 'POST',
+                processData: false,
+                contentType: "application/json; charset=utf-8",
+                dataType: 'json',
+                data: JSON.stringify({
+                    title: $(this).find('[name="title"]').val(),
+                    signature: $(this).find('[name="signature"]').val()
+                }),
+                success: function(data) {
+                    console.log(data);
+                    alert('Book created');
+                }
+            });
+
+            return false;
+        }
+    );
+});
